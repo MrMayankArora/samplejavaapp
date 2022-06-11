32fe595170d14bc332f476562a60d3ecb4d958c9
MrMayankArora <73720979+MrMayankArora@users.noreply.github.com>
2022-06-11 12:08:51 +0530

    Update Contact.java

--- src/main/java/com/devopsdemo/tutorial/addressbook/backend/Contact.java
+++ src/main/java/com/devopsdemo/tutorial/addressbook/backend/Contact.java
@@ -6,7 +6,7 @@ import java.io.Serializable;
 import java.util.Date;
 
 /**
- * A simple DTO for the address book example.
+ * A simple DTO for the address book example. . . . . . . .  x x x x x x x x x x x x x x x x x x
  *
  * Serializable and cloneable Java Object that are typically persisted
  * in the database and can also be easily converted to different formats like JSON.
